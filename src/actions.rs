4611c7afb89c81a49f303253dd8270c4898abfc4
tecuc <tecuceanu.gabriel22@gmail.com>
2025-06-12 15:26:08 +0300

    Add error handling with anyhow
    - drop `TmuxError` type
    
    Signed-off-by: tecuc <tecuceanu.gabriel22@gmail.com>

--- src/actions.rs
+++ src/actions.rs
@@ -1,6 +1,4 @@
-use std::{fs, path};
-
-use serde::Serialize;
+use anyhow::{Context, Result};
 
 use crate::cli::{Args, Commands};
 use crate::persistence::*;
@@ -8,39 +6,44 @@ use crate::tmux_interface::*;
 
 const BASE_PATH: &str = "";
 
-fn new(_session_name: &str) {
+fn new(_session_name: &str) -> Result<()> {
     todo!();
 }
 
-fn save(session_name: &str) -> Result<(), TmuxError> {
-    let mut current_session = get_session()?;
+fn save(session_name: &str) -> Result<()> {
+    let mut current_session =
+        get_session().context("Failed to get current session")?;
+
     current_session.name = session_name.to_string();
 
-    let yaml = serde_yaml::to_string(&current_session).unwrap();
+    let yaml = serde_yaml::to_string(&current_session).with_context(|| {
+        format!("Failed to serialize session {:#?} to yaml", current_session)
+    })?;
 
-    save_session_config(session_name, yaml);
+    save_session_config(session_name, yaml)
+        .context("Failed to save yaml config to disk")?;
 
     Ok(())
 }
 
-fn open(_session_name: &str) {
+fn open(_session_name: &str) -> Result<()> {
     todo!();
 }
 
-fn edit(_session_name: &str) {
+fn edit(_session_name: &str) -> Result<()> {
     todo!();
 }
 
-fn menu() {
+fn menu() -> Result<()> {
     todo!();
 }
 
-pub fn handle(args: Args) -> Result<(), TmuxError> {
+pub fn handle(args: Args) -> Result<()> {
     match args.command {
-        Commands::New { session_name } => Ok(new(&session_name)),
+        Commands::New { session_name } => new(&session_name),
         Commands::Save { session_name } => save(&session_name),
-        Commands::Open { session_name } => Ok(open(&session_name)),
-        Commands::Edit { session_name } => Ok(edit(&session_name)),
-        Commands::Menu => Ok(menu()),
+        Commands::Open { session_name } => open(&session_name),
+        Commands::Edit { session_name } => edit(&session_name),
+        Commands::Menu => menu(),
     }
 }
--- src/persistence.rs
+++ src/persistence.rs
@@ -1,39 +1,40 @@
-use dirs::home_dir;
 use std::fs;
 use std::path::PathBuf;
+
+use anyhow::{Context, Result};
+use dirs::home_dir;
 
 use crate::tmux_interface::*;
 
 const CONFIG_DIR: &str = ".tsessions";
 
-fn get_config_file_path(file_name: &str) -> Result<PathBuf, TmuxError> {
-    let mut path = home_dir().ok_or(TmuxError::InvalidOutputFormat(
-        "Could not determine Home Dir".to_string(), // TODO: use appropriate error type
-    ))?;
+fn get_config_file_path(file_name: &str) -> Result<PathBuf> {
+    let mut path = home_dir()
+        .ok_or_else(|| anyhow::anyhow!("Failed to determine HOME directory"))?;
+
     path.push(".config");
     path.push(CONFIG_DIR);
     path.push(format!("{}.yaml", file_name));
     Ok(path)
 }
 
-pub fn save_session_config(
-    file_name: &str,
-    data: String,
-) -> Result<(), TmuxError> {
+pub fn save_session_config(file_name: &str, data: String) -> Result<()> {
     let path = get_config_file_path(file_name)?;
 
     if let Some(parent) = path.parent() {
-        fs::create_dir_all(parent)?;
+        fs::create_dir_all(parent).with_context(|| {
+            format!("failed to create directory {}", parent.display())
+        })?;
     }
 
     fs::write(&path, data)?;
     Ok(())
 }
 
-pub fn load_session_from_config(file_name: &str) -> Result<Session, TmuxError> {
+pub fn load_session_from_config(file_name: &str) -> Result<Session> {
     todo!();
 }
 
-pub fn list_saved_sessions() -> Result<Vec<String>, TmuxError> {
+pub fn list_saved_sessions() -> Result<Vec<String>> {
     todo!();
 }
