b49b32187d877648a7e97c21fc59c77b47d9d830
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-07-29 22:18:35 +0300

    refactor: extract fzf query code to separate function
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- src/actions.rs
+++ src/actions.rs
@@ -66,6 +66,11 @@ fn edit(_session_name: &str) -> Result<()> {
 }
 
 fn menu() -> Result<()> {
+    open(&querry_sessions_with_fzf()?)?;
+    Ok(())
+}
+
+fn querry_sessions_with_fzf() -> Result<String> {
     let file_names = list_saved_sessions()?;
 
     let mut child = Command::new("fzf")
@@ -81,9 +86,5 @@ fn menu() -> Result<()> {
 
     let output = child.wait_with_output().expect("Failed to read output");
 
-    let file_name = String::from_utf8(output.stdout)?.trim().to_string();
-
-    open(&file_name)?;
-
-    Ok(())
+    Ok(String::from_utf8(output.stdout)?.trim().to_string())
 }
