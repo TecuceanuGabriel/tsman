55bf6320b63c8712ee825a97d2423d7469578936
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-08-04 10:58:00 +0300

    feat(config): make session config storage dir configurable
    - load the config dir from `$TSMAN_CONFIG_STORAGE_DIR` if set, otherwise
      default to `~/.config/.tsessions`.
    - introduce get_and_ensure_session_storage_dir() which makes sure the
      directory is created before returning it.
    
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- src/persistence.rs
+++ src/persistence.rs
@@ -1,20 +1,13 @@
-use std::fs;
 use std::path::PathBuf;
+use std::{env, fs};
 
 use anyhow::{Context, Result};
 use dirs::home_dir;
 
-const CONFIG_DIR: &str = ".tsessions"; // TODO: make this configurable
+const DEFAULT_CONFIG_STORAGE_DIR: &str = ".tsessions";
 
 pub fn save_session_config(file_name: &str, data: String) -> Result<()> {
     let path = get_config_file_path(file_name)?;
-
-    if let Some(parent) = path.parent() {
-        fs::create_dir_all(parent).with_context(|| {
-            format!("Failed to create directory {}", parent.display())
-        })?;
-    }
-
     fs::write(&path, data)?;
     Ok(())
 }
@@ -26,7 +19,7 @@ pub fn load_session_from_config(file_name: &str) -> Result<String> {
 }
 
 pub fn list_saved_sessions() -> Result<Vec<String>> {
-    let dir_path = get_default_session_dir_path()?;
+    let dir_path = get_and_ensure_session_storage_dir()?;
 
     let paths = fs::read_dir(dir_path.into_os_string())?;
     let mut result = Vec::with_capacity(paths.size_hint().0);
@@ -49,17 +42,25 @@ pub fn list_saved_sessions() -> Result<Vec<String>> {
 }
 
 pub fn get_config_file_path(file_name: &str) -> Result<PathBuf> {
-    let mut path = get_default_session_dir_path()?;
+    let mut path = get_and_ensure_session_storage_dir()?;
     path.push(format!("{}.yaml", file_name));
     Ok(path)
 }
 
-fn get_default_session_dir_path() -> Result<PathBuf> {
-    let mut path = home_dir()
+fn get_and_ensure_session_storage_dir() -> Result<PathBuf> {
+    let dir_path = get_session_storage_dir_path()?;
+    fs::create_dir_all(&dir_path).with_context(|| {
+        format!("Failed to create directory {}", dir_path.display())
+    })?;
+    Ok(dir_path)
+}
+
+fn get_session_storage_dir_path() -> Result<PathBuf> {
+    if let Ok(dir) = env::var("TSMAN_CONFIG_STORAGE_DIR") {
+        return Ok(PathBuf::from(dir));
+    }
+
+    let home = home_dir()
         .ok_or_else(|| anyhow::anyhow!("Failed to determine HOME directory"))?;
-
-    path.push(".config");
-    path.push(CONFIG_DIR);
-
-    Ok(path)
+    Ok(home.join(".config").join(DEFAULT_CONFIG_STORAGE_DIR))
 }
