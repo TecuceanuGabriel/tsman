5aa7b50447a8ea4cd8f9a9d309b91d6b77409245
Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>
2025-08-10 20:40:55 +0300

    feat(cli): improve cli descriptions
    Signed-off-by: Gabriel-Cristian Tecuceanu <tecuceanu.gabriel22@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,24 @@ use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 #[command(name = "tsman")]
-#[command(about = "A session manager for tmux", long_about = None)]
+#[command(
+    about = "A session manager for tmux",
+    long_about = "tsman - A lightweight session manager for tmux.
+
+Key Features:
+ - Quickly save/restore/delete sessions. 
+ - Manage sessions from the interactive TUI menu.
+ - Manually edit saved session for better control.
+
+Examples:
+ tsman save my-session # save the current session as `my-session`
+ tsman edit my-session # edit `my-session` for your liking
+ tsman open my-session # restore `my-session`
+ tsman menu -p -a      # open the TUI menu with the preview panel and 
+                       # delete confirmation prompting on
+
+Use `tsman <COMMAND> --help` for more details."
+)]
 pub struct Args {
     #[command(subcommand)]
     pub command: Commands,
@@ -10,25 +27,61 @@ pub struct Args {
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    /// save the current session
-    Save { session_name: Option<String> },
+    #[command(
+        about = "Save the current session",
+        long_about = "Save the currently attached tmux session. Stores the 
+<session_name>.yaml config file in $TSMAN_CONFIG_STORAGE_DIR if set, or 
+~/.config/.tsessions."
+    )]
+    Save {
+        /// Name of the session (default: name of current session)
+        session_name: Option<String>,
+    },
 
-    /// open the selected session
-    #[command(arg_required_else_help = true)]
-    Open { session_name: String },
+    #[command(
+        about = "Open the specified session",
+        long_about = "Restore the selected session and then attach to it.",
+        arg_required_else_help = true
+    )]
+    Open {
+        /// Name of the session
+        session_name: String,
+    },
 
-    /// open the config file of the selected session in $EDITOR
-    Edit { session_name: Option<String> },
+    #[command(
+        about = "Edit the specified session",
+        long_about = "Open the config file of the specified session in $EDITOR
+for manual editing."
+    )]
+    Edit {
+        /// Name of the session (default: name of current session)
+        session_name: Option<String>,
+    },
 
-    /// delete the config file of the selected session
-    #[command(arg_required_else_help = true)]
-    Delete { session_name: String },
+    #[command(
+        about = "Delete specified session",
+        long_about = "Remove the config file of the specified session from the
+config storage directory.",
+        arg_required_else_help = true
+    )]
+    Delete {
+        /// Name of the session
+        session_name: String,
+    },
 
-    /// display menu containing all sessions
+    #[command(
+        about = "Open up a menu containing all sessions",
+        long_about = "Open up an interactive menu containing all saved or 
+currently active sessions."
+    )]
     Menu {
         #[clap(long, short, help = "Show preview pane on start up")]
         preview: bool,
-        #[clap(long, short, help = "Ask for confirmation before deleting")]
+        #[clap(
+            long,
+            short,
+            help = "Prompt for confirmation before deleting a session"
+        )]
         ask_for_confirmation: bool,
     },
 }
